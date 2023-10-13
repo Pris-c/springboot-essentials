c99cd8141613b15c1cdbfeb7af1a133b34c92dce
Priscila <priscila.campoos@yahoo.com.br>
2023-10-13 15:56:17 +0100

    Implemented method findByName using @RequestParam

--- src/main/java/com/prisc/springboot/controller/AnimeController.java
+++ src/main/java/com/prisc/springboot/controller/AnimeController.java
@@ -33,8 +33,13 @@ public class AnimeController {
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
-        log.info(dateUtil.formatLocalDateTimeToDataBaseStile(LocalDateTime.now()));
+        //log.info(dateUtil.formatLocalDateTimeToDataBaseStile(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
+    }
+    
+    @GetMapping(path = "/find")
+    public ResponseEntity<List<Anime>> findByName(@RequestParam String name ){
+        return ResponseEntity.ok(animeService.findByName(name));
     }
 
     @PostMapping
--- src/main/java/com/prisc/springboot/repository/AnimeRepository.java
+++ src/main/java/com/prisc/springboot/repository/AnimeRepository.java
@@ -3,6 +3,10 @@ package com.prisc.springboot.repository;
 import com.prisc.springboot.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findByName(String name);
 
 }
